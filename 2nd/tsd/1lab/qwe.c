a93b49b38e83de632c60dacff86534675629de68
Irina <irinqut@gmail.com>
2016-10-05 00:28:07 +0300

    reservecopy 1st lab tsd

--- 2nd/tsd/1lab/qwe.c
+++ 2nd/tsd/1lab/qwe.c
@@ -39,43 +39,42 @@ int main(void)
 	int count_of_dig_float = 0;
 	float_array_generate(float_num, float_number, &exponent, &count_of_dig_float, &flag_point, &flag_e);
 
-	if (exponent >= -99999)
-	{
-		printf("Знак = %c \n",znak_float);
-		printf("Число = ");
-		for (int i = 0; i < count_of_dig_float ;i++)
-			printf("%d",float_number[i]);
-		printf("\nСтепень = %d\n",exponent);
-		
-		int result[60] = {0};
-		int flag = 0;
-		if (count_of_dig_int > count_of_dig_float)
-			counting(integer_number, count_of_dig_int, float_number, count_of_dig_float, result, &flag);
-		else
-			counting(float_number, count_of_dig_float, integer_number, count_of_dig_int, result, &flag);
+	printf("Знак = %c \n",znak_float);
+	printf("Число = ");
+	for (int i = 0; i < count_of_dig_float ;i++)
+		printf("%d",float_number[i]);
+	printf("\nСтепень = %d\n",exponent);
+	
+	int result[60] = {0};
+	int flag = 0;
+	if (count_of_dig_int > count_of_dig_float)
+		counting(integer_number, count_of_dig_int, float_number, count_of_dig_float, result, &flag);
+	else
+		counting(float_number, count_of_dig_float, integer_number, count_of_dig_int, result, &flag);
 
-		if (znak_int == znak_float)
-			znak_result = '+';
-		else 
-			znak_result = '-';
+	if (znak_int == znak_float)
+		znak_result = '+';
+	else 
+		znak_result = '-';
 
-		// printf("[DEBUG INF]Result of counting = ");
-		// printf("%c",znak_result);
-		// for (int i = 0; i < count_of_dig_int+count_of_dig_float; i++)
-		// 	printf("%d",result[i]);
-		// printf("E%d",exponent-flag);
-		// printf("\n");
+	// printf("[DEBUG INF]Result of counting = ");
+	// printf("%c",znak_result);
+	// for (int i = 0; i < count_of_dig_int+count_of_dig_float; i++)
+	// 	printf("%d",result[i]);
+	// printf("E%d",exponent-flag);
+	// printf("\n");
 
-		int result_with_pointer[30] = {0};
-		normalize(result, result_with_pointer, &exponent, count_of_dig_int+count_of_dig_float, &flag);
-		if (exponent >= -99999 && exponent <= 99999)
-			print_float(znak_result, result_with_pointer, exponent, count_of_dig_float+count_of_dig_int);
-		else
-			printf("Ошибка вычисления\n");
-
-	}
+	int result_with_pointer[30] = {0};
+	normalize(result, result_with_pointer, &exponent, count_of_dig_int+count_of_dig_float, &flag);
+	if (exponent >= -99999 && exponent <= 99999)
+		print_float(znak_result, result_with_pointer, exponent, count_of_dig_float+count_of_dig_int);
 	else
 		printf("Ошибка вычисления\n");
+
 }
+
+// ПОЧИНИТЬ ПОРЯДОК ПРИ ОКРУГЛЕНИИ
+// ПРОВЕРИТЬ 100 0.01Е-99999 
+// ПЕРЕНЕСТИ ВНУТРЕННЮЮ СТРУКТУРУ В ОТЧЕТЕ
 
 
